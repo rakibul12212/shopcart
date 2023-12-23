a85d37194fd40eced2002ede45ba12d3fde5b483
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-12-23 04:42:14 -0800

    navber done

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -100,9 +100,12 @@ const Navbar = () => {
       <nav className="bg-white p-4">
         <div className="container mx-auto lg:flex lg:items-center lg:justify-between lg:gap-4">
           <div className="flex justify-between">
+            {/* logo */}
             <div className="text-green-900 text-2xl font-bold" id="logo">
               <a href="#">ShopCart</a>
             </div>
+
+            {/* search */}
 
             {/* Mobile Toggler */}
             <div
@@ -126,24 +129,74 @@ const Navbar = () => {
           </div>
 
           {/* Mobile Menu */}
-          <div
-            className={`mobile-menu lg:hidden ${
-              mobileMenuOpen ? "block" : "hidden"
-            }`}
-          >
-            <a href="#" className="block py-2">
-              Category
-            </a>
-            <a href="#" className="block py-2">
-              Deals
-            </a>
-            <a href="#" className="block py-2">
-              What&apos;s New
-            </a>
-            <a href="#" className="block py-2">
-              Delivery
-            </a>
-            {/* Add more mobile menu items as needed */}
+          <div>
+            <div
+              className={`mobile-menu lg:hidden ${
+                mobileMenuOpen ? "block" : "hidden"
+              }`}
+            >
+              <div className=" py-4 ">
+                <div>
+                  {" "}
+                  <span className="font-bold">Category</span>
+                  <select className="px-5">
+                    <option value="subcategory1">Toy</option>
+                    <option value="subcategory2">Electonics</option>
+                    <option value="subcategory3">bag</option>
+                  </select>
+                </div>
+                <div>
+                  <a href="#" className="font-bold">
+                    Deals
+                  </a>
+                </div>
+                <div>
+                  <a href="#" className="font-bold">
+                    Whats&apos;s New
+                  </a>
+                </div>
+                <div>
+                  <a href="#" className="font-bold">
+                    Delivery
+                  </a>
+                </div>
+
+                <div className="flex space-x-3">
+                  <a href="#">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                      />
+                    </svg>
+                  </a>
+                  <a href="#">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
 
           {/* Desktop Menu */}
