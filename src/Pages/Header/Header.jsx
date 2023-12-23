ef6c463562da74b5a03c63e9b5be0e326015c7a5
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-12-23 12:02:51 -0800

    make header left responsive

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -3,19 +3,19 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="bg ">
-      <div id="header-left" className="container py-20">
-        <h1 className="font-bold text-8xl w-1/2 items-center tracking-wide text-green-800">
+    <div className="bg">
+      <div id="header-left" className="container py-8 sm:py-20">
+        <h1 className="font-bold text-4xl sm:text-7xl w-full sm:w-1/2 items-center tracking-wide text-green-800 ">
           <span>Shopping And</span>
           <br /> <span>Department Store.</span>
         </h1>
-        <p className="text-2xl w-1/3 py-12 text-neutral-500">
-          Shopping is a bit of a relexing hobby which is sometimes troubling for
-          the bank balence.
+        <p className="text-lg sm:text-2xl w-full sm:w-1/2 py-4 sm:py-12 text-neutral-500 ">
+          Shopping is a bit of a relaxing hobby which is sometimes troubling for
+          the bank balance.
         </p>
-        <a className="btn" href="/">
-          Learn More
-        </a>
+        <button id="btn">
+          <a href="#">Learn More</a>
+        </button>
       </div>
     </div>
   );
