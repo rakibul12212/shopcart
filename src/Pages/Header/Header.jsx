77e9f163ea10d9b75ba7e79d202878c5e059dd1e
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-12-23 05:34:32 -0800

    createing header file

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -3,8 +3,20 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <p>this is header</p>
+    <div className="bg ">
+      <div id="header-left" className="container py-20">
+        <h1 className="font-bold text-8xl w-1/2 items-center tracking-wide text-green-800">
+          <span>Shopping And</span>
+          <br /> <span>Department Store.</span>
+        </h1>
+        <p className="text-2xl w-1/3 py-12 text-neutral-500">
+          Shopping is a bit of a relexing hobby which is sometimes troubling for
+          the bank balence.
+        </p>
+        <a className="btn" href="/">
+          Learn More
+        </a>
+      </div>
     </div>
   );
 };
